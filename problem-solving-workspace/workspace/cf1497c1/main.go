a358fd96b3dd654a4a9f4a125f5814b8a222228e
Rafael Niquefa <niquefa@gmail.com>
2023-09-05 13:02:10 -0500

    1 more go problem

---
+++ problem-solving-workspace/workspace/cf1497c1/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+var (
+	in  = bufio.NewReader(os.Stdin)
+	out = bufio.NewWriter(os.Stdout)
+)
+
+func flush()                            { out.Flush() }
+func scan(a ...interface{})             { fmt.Fscan(in, a...) }
+func printf(f string, a ...interface{}) { fmt.Fprintf(out, f, a...) }
+func debug(a ...interface{})            { fmt.Fprintln(os.Stderr, a...) }
+func assert(f bool) {
+	if !f {
+		panic("gg Rafa, something went wrong!")
+	}
+}
+
+// ----------------------------- TEMPLATE END ----------------------------------
+
+func solve() {
+	var n, k int
+	scan(&n, &k)
+	var half = n / 2
+	if n%2 == 1 {
+		fmt.Println(1, half, half)
+		return
+	}
+	if n%4 == 0 {
+		fmt.Println(half, half/2, half/2)
+		return
+	}
+	fmt.Println(half-1, half-1, 2)
+
+}
+
+func main() {
+	startTime := time.Now()
+
+	defer flush()
+
+	var ntc int = 1
+	scan(&ntc)
+	for t := 0; t < ntc; t++ {
+		debug("\n====================================", t, "====================================")
+		solve()
+	}
+
+	debug(fmt.Sprintf("\n\t\t>>> %s EXECUTION TIME <<<\n", time.Since(startTime)))
+}
