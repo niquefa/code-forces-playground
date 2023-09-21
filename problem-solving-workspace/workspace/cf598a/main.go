79155a8de0079a6e852d9c79afbdbfe2e16c6968
Rafael Niquefa <niquefa@gmail.com>
2023-09-20 22:14:25 -0500

    Just one problem

---
+++ problem-solving-workspace/workspace/cf598a/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+var (
+	in  = bufio.NewReader(os.Stdin)
+	out = bufio.NewWriter(os.Stdout)
+)
+
+func flush()                            { out.Flush() }
+func scan(a ...interface{})             { fmt.Fscan(in, a...) }
+func printf(f string, a ...interface{}) { fmt.Fprintf(out, f, a...) }
+func debug(a ...interface{})            { fmt.Fprintln(os.Stderr, a...) }
+func assert(f bool) {
+	if !f {
+		panic("gg Rafa, something went wrong!")
+	}
+}
+
+// ----------------------------- TEMPLATE END ----------------------------------
+
+func solve() {
+	var n int64
+	scan(&n)
+	var s int64 = (n * (n + 1)) / 2
+	var p int64 = 1
+	for p <= n {
+		s -= 2 * p
+		p *= 2
+	}
+
+	fmt.Println(s)
+
+}
+
+func main() {
+	startTime := time.Now()
+
+	defer flush()
+
+	var ntc int = 1
+	scan(&ntc)
+	for t := 0; t < ntc; t++ {
+		debug("\n====================================", t, "====================================")
+		solve()
+	}
+
+	debug(fmt.Sprintf("\n\t\t>>> %s EXECUTION TIME <<<\n", time.Since(startTime)))
+}
